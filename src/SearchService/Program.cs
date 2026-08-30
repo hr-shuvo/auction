8123c79469fb7d3eb5b3244f5ca239cf17a74f10
H R Shuvo <h.r.shuvo9@gmail.com>
2026-08-30 16:58:31 +0600

    Add MongoDB integration with Item model and update configuration

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -6,5 +10,14 @@ var app = builder.Build();
 
 
 app.MapControllers();
+
+var db =await DB.InitAsync("SearchDb",
+    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("SearchDbConnection")));
+
+await db.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
