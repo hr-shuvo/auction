038512c7a6aa18e73624ac03c1ff4f9763f97354
H R Shuvo <h.r.shuvo9@gmail.com>
2026-08-31 08:09:22 +0600

    Implement Search functionality with SearchController, DbInitializer, and SearchParams model

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Data;
+
+public static class DbInitializer
+{
+    public static async Task Initialize(this IServiceCollection services, IConfiguration config)
+    {
+        var db = await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(config.GetConnectionString("SearchDbConnection")));
+
+
+        services.AddSingleton(db);
+
+        await db.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+
+        var count = await db.CountAsync<Item>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("Creating items...");
+
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await db.SaveAsync(items);
+        }
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,23 +1,23 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
-using SearchService.Models;
+using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+try
+{
+    await builder.Services.Initialize(builder.Configuration);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 var app = builder.Build();
 
 
 app.MapControllers();
 
-var db =await DB.InitAsync("SearchDb",
-    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("SearchDbConnection")));
 
-await db.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
 
 app.Run();
